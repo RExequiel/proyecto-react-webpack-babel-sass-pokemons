cf4f94f2a97a4ede2745c4f4f19def517ae18220
Exequiel <exe2016gerez@gmail.com>
2022-06-23 19:26:38 -0300

    agregando hook personalizado

--- src/views/AppPokemons.jsx
+++ src/views/AppPokemons.jsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from "react";
+import { useFetch } from "../hooks/useFetch";
 import Pokemon from "../components/Pokemon";
-import '../sass/AppPokemons.scss';
-
+import "../sass/AppPokemons.scss";
 
 const AppPokemons = () => {
+  let url = "https://pokeapi.co/api/v2/pokemon/";
+  //console.log(useFetch());
+
+  let { data, isPending, error } = useFetch(url);
+
   const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    const getPokemons = async (url) => {
-      let res = await fetch(url),
-        json = await res.json();
-      //console.log(json);
+    const getPokemons = async () => {
+      let res = await fetch(url);
+      data = await res.json();
+      // console.log(data);
 
-      json.results.forEach(async (el) => {
+      data.results.forEach(async (el) => {
         let res = await fetch(el.url),
           json = await res.json();
 
@@ -27,12 +32,30 @@ const AppPokemons = () => {
       });
     };
 
-    getPokemons("https://pokeapi.co/api/v2/pokemon/");
-  }, []);
-
+    getPokemons();
+  }, [url]);
   return (
     <>
       <h1 className="title">Pokemons name</h1>
+
+      <div style={{ border: "1px solid #48e" }}>
+        <h2 className="loading">Estado de la petición Ajax</h2>
+        <h3>Pendiente: {JSON.stringify(isPending)}</h3>
+        <br />
+        <h3>
+          <mark>Error: {JSON.stringify(error)}</mark>
+        </h3>
+        <br />
+        <pre style={{ whiteSpace: "pre-wrap" }}>
+          <h3>Datos:</h3>
+          <br />
+          <code style={{ wordBreak: "break-word" }}>
+            {JSON.stringify(data)}
+          </code>
+        </pre>
+        <br />
+      </div>
+
       {pokemons.length === 0 ? (
         <h2 className="loading">Cargando...</h2>
       ) : (
